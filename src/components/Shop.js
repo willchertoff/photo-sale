3043c6a790fb31cfa5026459ce235d11f8171dbe
Will Chertoff <willchertoff@gmail.com>
2016-11-08 12:39:00 -0800

    feat(shop): Update Component: Shop refactor

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,19 +1,32 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
 const propTypes = {
-  panel: PropTypes.string.isRequired,
+  prints: PropTypes.array.isRequired,
+  total: PropTypes.number.isRequired,
+  onCheckoutClick: PropTypes.func.isRequired,
 };
-
-class Shop extends Component {
-  render() {
-    const { panel } = this.props;
-    return (
-      <div className="panel">
-        <p>{panel}</p>
-      </div>
-    );
-  }
-}
+const Shop = ({ prints, total, onCheckoutClick }) => {
+  const hasPrints = prints.length > 0;
+  const message = hasPrints ? (
+    <p>{`${prints.length} x Prints = ${total} $`}</p>
+  ) : (
+  <p>You don't have any prints selected</p>
+  );
+  const title = hasPrints ? (
+    <div>
+      <h3>Cart</h3>
+      <button onClick={onCheckoutClick}>Checkout</button>
+    </div>
+  ) : (
+    ''
+  );
+  return (
+    <div className="panel">
+      {title}
+      {message}
+    </div>
+  );
+};
 
 Shop.propTypes = propTypes;
 
--- src/containers/ShopContainer.js
+++ src/containers/ShopContainer.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Shop from '../components/Shop';
 
+/* To replace with reducer data */
+const { prints, total, onCheckoutClick } = {
+  prints: [{ imageId: 1 }, { imageId: 2 }],
+  total: 10,
+  onCheckoutClick: () => { /* do nothing */ },
+};
+
 class ShopContainer extends Component {
   render() {
     return (
-      <Shop {...this.props} />
+      <Shop {...this.props} prints={prints} total={total} onCheckoutClick={onCheckoutClick} />
     );
   }
 }
