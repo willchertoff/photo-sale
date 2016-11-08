5c8ffacf235f73bd4b822d082f4724acfcfc0203
Will Chertoff <willchertoff@gmail.com>
2016-11-08 12:59:13 -0800

    feat(stream): Update Component -- Update Stream to have simple component form

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -1,29 +1,26 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import Masonry from 'react-masonry-component';
 
 const propTypes = {
   images: PropTypes.array.isRequired,
 };
 
-class Stream extends Component {
-  render() {
-    const { images } = this.props;
-    const masonryImages = images.map(image => (
-      <li key={image.imageId} className="image-element-class">
-        <img src={image.url} alt={image.Id} />
-      </li>
-    ));
-    return (
-      <div className="panel">
-        <Masonry
-          className={'my-gallery-class'}
-        >
-          {masonryImages}
-        </Masonry>
-      </div>
-    );
-  }
-}
+const Stream = ({ images }) => {
+  const masonryImages = images.map(image => (
+    <li key={image.imageId} className="image-element-class">
+      <img src={image.url} alt={image.Id} />
+    </li>
+  ));
+  return (
+    <div className="panel">
+      <Masonry
+        className={'my-gallery-class'}
+      >
+        {masonryImages}
+      </Masonry>
+    </div>
+  );
+};
 
 Stream.propTypes = propTypes;
 
