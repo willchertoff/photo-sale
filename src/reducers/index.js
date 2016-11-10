bd2f1d0442064fe29c2e8c38e96f67083f5d37d5
Will Chertoff <willchertoff@gmail.com>
2016-11-10 09:43:12 -0800

    chore(reducer): Remove Messages Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import message from '../reducers/message';
 
 const rootReducer = combineReducers({
   routing: routerReducer,
-  message,
 });
 
 export default rootReducer;
