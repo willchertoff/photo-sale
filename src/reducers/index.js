83793f82913857325ddd1f056c4d28020d4b11ae
Will Chertoff <willchertoff@gmail.com>
2016-11-10 10:03:31 -0800

    feat(blog): Update Component -- Add posts reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import posts from './posts';
 
 const rootReducer = combineReducers({
+  posts,
   routing: routerReducer,
 });
 
