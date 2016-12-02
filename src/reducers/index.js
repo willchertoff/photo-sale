9d18f846a20c40dfb9a30934a1b5b692ab43488c
Will Chertoff <willchertoff@gmail.com>
2016-12-02 12:35:50 -0800

    feat(mobile): Add Reducer -- Environment Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import environment from './environment';
 import images from './images';
 import posts from './posts';
 
 const rootReducer = combineReducers({
+  environment,
   images,
   posts,
   routing: routerReducer,
