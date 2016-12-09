1d6baf2723f00e03e2bc3657390ce440e558be7e
Will Chertoff <willchertoff@gmail.com>
2016-12-08 16:59:54 -0800

    feat(blog): Adjust BlogArticle Styles

--- src/components/BlogArticle.js
+++ src/components/BlogArticle.js
@@ -4,19 +4,19 @@ import { connect } from 'react-redux';
 const propTypes = {
   coverImage: PropTypes.any.isRequired,
   title: PropTypes.string.isRequired,
-  publishDate: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
 };
 
 class BlogArticle extends Component {
 
   render() {
-    const { coverImage, title, publishDate, body } = this.props;
+    const { coverImage, title, body, subtitle } = this.props;
     return (
       <div className="blog-article">
-        <img src={`/images/${coverImage}.JPG`} alt={`${title}'s feature`} />
         <h1 className="blog-title" >{title}</h1>
-        <h5>{publishDate}</h5>
+        <h2 className="blog-subtitle" >{subtitle}</h2>
+        <img src={`/images/${coverImage}.JPG`} alt={`${title}'s feature`} />
         <div className="blog-body" dangerouslySetInnerHTML={{ __html: body }} />
       </div>
     );
@@ -28,13 +28,14 @@ function mapStateToProps(state, { params }) {
   const { posts } = state;
   const { items } = posts;
   const post = postId ? items[postId - 1] : null;
-  const { coverImage, title, publishDate, body } = post;
+  const { coverImage, title, publishDate, body, subtitle } = post;
 
   return {
     coverImage,
     title,
     publishDate,
     body,
+    subtitle,
   };
 }
 
