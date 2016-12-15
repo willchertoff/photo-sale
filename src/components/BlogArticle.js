89a954ebefd21d0ac0d6f9558275eab491baedcd
Will Chertoff <willchertoff@gmail.com>
2016-12-15 11:22:23 -0800

    feat(share-buttons): Add share buttons to blog articles

--- src/components/BlogArticle.js
+++ src/components/BlogArticle.js
@@ -1,18 +1,33 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import LazyLoad from 'react-lazyload';
+import { ShareButtons, generateShareIcon } from 'react-share';
+
+const {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+} = ShareButtons;
+
+const FacebookIcon = generateShareIcon('facebook');
+const TwitterIcon = generateShareIcon('twitter');
+const GooglePlusIcon = generateShareIcon('google');
+const LinkedinIcon = generateShareIcon('linkedin');
 
 const propTypes = {
   coverImage: PropTypes.any.isRequired,
   title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
 };
 
 class BlogArticle extends Component {
 
   render() {
-    const { coverImage, title, body, subtitle } = this.props;
+    const { coverImage, title, body, subtitle, location } = this.props;
+    const { pathname } = location;
     return (
       <div className="blog-article">
         <h1 className="blog-title" >{title}</h1>
@@ -21,6 +36,20 @@ class BlogArticle extends Component {
           <img src={`/images/${coverImage}.JPG`} alt={`${title}'s feature`} />
         </LazyLoad>
         <div className="blog-body" dangerouslySetInnerHTML={{ __html: body }} />
+        <div className="social">
+          <TwitterShareButton url={`http://willchertoff.photos${pathname}`}>
+            <TwitterIcon size={32} />
+          </TwitterShareButton>
+          <FacebookShareButton url={`http://willchertoff.photos${pathname}`}>
+            <FacebookIcon size={32} />
+          </FacebookShareButton>
+          <LinkedinShareButton url={`http://willchertoff.photos${pathname}`}>
+            <LinkedinIcon size={32} />
+          </LinkedinShareButton>
+          <GooglePlusShareButton url={`http://willchertoff.photos${pathname}`}>
+            <GooglePlusIcon size={32} />
+          </GooglePlusShareButton>
+        </div>
       </div>
     );
   }
